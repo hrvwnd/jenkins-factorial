2a1ef0f6e76a87a6730b717278f2e27fd2a12293
hrvwnd <harveyawendon@gmail.com>
2019-11-26 14:47:30 +0000

    working code

--- factorial.py
+++ factorial.py
@@ -14,10 +14,13 @@ def reverse_factorial(number):
     if number ==0:
         return number 
     else: 
-        while number != 1
-        for i in range(1,count):
-            print ("i is: ", i)
-            number /=i
-        return number 
+        while number > 1:
+            for i in range(1,(count+1)):
+                print ("i is: ", i)
+                number /=i
+                if number ==1:
+                    return i
+            
+        return "None" 
 
 
--- test_factorial.py
+++ test_factorial.py
@@ -14,4 +14,11 @@ def test_factorial_200():
 def test_reverse_factorial_120():
     assert factorial.reverse_factorial(120) == 5
 
+def test_reverse_factorial_none():
+    assert factorial.reverse_factorial(23) == "None"
+
+
+def test_reverse_factorial_24():
+    assert factorial.reverse_factorial(24) == 4
+
 
