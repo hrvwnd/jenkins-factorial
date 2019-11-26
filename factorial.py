5ca2507f3acac6cab6cd913ad29645a2f4ac5b2b
hrvwnd <harveyawendon@gmail.com>
2019-11-26 14:04:36 +0000

    factorial code - with complete pt 1

---
+++ factorial.py
@@ -0,0 +1,23 @@
+def factorial(number):
+    count = number
+    if number == 0:
+        return number
+    else:
+        for i in range(1,count):
+            print ("i is: " ,i)
+            number *= i
+            print (number)
+        return number
+
+def reverse_factorial(number):
+    count = number
+    if number ==0:
+        return number 
+    else: 
+        while number != 1
+        for i in range(1,count):
+            print ("i is: ", i)
+            number /=i
+        return number 
+
+
---
+++ test_factorial.py
@@ -0,0 +1,17 @@
+
+import pytest
+import factorial
+
+def test_factorial_zeros():
+    assert factorial.factorial(0)==0
+
+def test_factorial_five():
+    assert factorial.factorial(5)==120
+
+def test_factorial_200():
+    assert factorial.factorial(20)== 2432902008176640000
+
+def test_reverse_factorial_120():
+    assert factorial.reverse_factorial(120) == 5
+
+
